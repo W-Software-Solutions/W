f8664e82a188c84c714636f9d22dfdd409afc544
W-Software-Solutions <154895656+W-Software-Solutions@users.noreply.github.com>
2024-09-30 23:06:23 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://papaya-churros-86083a.netlify.app/",
     credentials: true,
 }))
 
