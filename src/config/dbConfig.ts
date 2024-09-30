9ac3ef56fd1829cb51866507a1006f983a832436
W-Software-Solutions <154895656+W-Software-Solutions@users.noreply.github.com>
2024-09-30 22:27:11 +0530

    Update dbConfig.ts

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try{
-        const connect = await mongoose.connect(`${process.env.CONNECTION_STRING}`);
-        // const connect = await mongoose.connect("mongodb+srv://anym696969:c6DcrlomHx2ntrH9@url-shortner-app.fe3g6.mongodb.net/?retryWrites=true&w=majority&appName=url-shortner-app");
+        // const connect = await mongoose.connect(`${process.env.CONNECTION_STRING}`);
+        const connect = await mongoose.connect("mongodb+srv://anym696969:c6DcrlomHx2ntrH9@url-shortner-app.fe3g6.mongodb.net/?retryWrites=true&w=majority&appName=url-shortner-app");
         console.log("Database Connected: ",
             connect.connection.host,
             connect.connection.name
